d07b387ad414c43d2daba7d6ebdb6edf44cfcd65
Ismail El Korchi <ismail.elkorchi@gmail.com>
2017-12-09 14:41:45 +0000

    Add content wrapper

--- footer.php
+++ footer.php
@@ -14,17 +14,19 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'guessclinic' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'guessclinic' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'guessclinic' ), 'guessclinic', '<a href="http://www.ismailelkorchi.com/">Ismail El Korchi</a>' );
-			?>
-		</div><!-- .site-info -->
+		<div class="wrap">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'guessclinic' ) ); ?>"><?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'guessclinic' ), 'WordPress' );
+				?></a>
+				<span class="sep"> | </span>
+				<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'guessclinic' ), 'guessclinic', '<a href="http://www.ismailelkorchi.com/">Ismail El Korchi</a>' );
+				?>
+			</div><!-- .site-info -->
+		</div><!-- .wrap -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
