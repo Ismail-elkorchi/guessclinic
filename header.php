125c9731b1d95c4bcfcc8396e7b1fb2cf8160849
Ismail El Korchi <ismail.elkorchi@gmail.com>
2017-12-11 15:48:20 +0000

    Basic styling for the header

--- header.php
+++ header.php
@@ -27,6 +27,11 @@
 	<header id="masthead" class="site-header">
 		<div class="wrap">
 			<div class="site-branding">
+				<div class="site-logo">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" />
+					</a>
+				</div>
 				<?php
 				the_custom_logo();
 				if ( is_front_page() && is_home() ) :
