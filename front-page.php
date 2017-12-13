c1b0b91017d69f1c5e91d429e014bea4d7f0c5bd
Ismail El Korchi <ismail.elkorchi@gmail.com>
2017-12-13 21:18:17 +0000

    Add hero section

---
+++ front-page.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The front page template file
+ *
+ * If the user has selected a static page for their homepage, this is what will
+ * appear.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Guess_Clinic
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<section class="hero">
+				<div class="wrap">
+					<div class="text-box">
+						<h2>La beauté n'a pas d'age </h2>
+						<p>
+							<span>Guess Clinic est un établissement de chirurgie esthétique, spécialisée dans la beauté, le bien-être et l'amincissement.</span>
+							<span>Découvrez une gamme d'offres de bien-être pour vous aider à renouveler et recharger vos batteries.</span>
+							<span>Commencez votre voyage de bien-être en guérissant votre corps, votre esprit et votre âme.</span>
+						</p>
+						<a href="#" class="button button-red">Découvrez notre clinique</a>
+					</div>
+				</div>
+
+
+			</section>
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
